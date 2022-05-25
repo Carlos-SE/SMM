e12fd4921a82ead7794c553a963b17b6011b9193
Carlos <carlossalaseiroa@gmail.com>
2022-05-25 12:09:32 +0200

    Initial commit

---
+++ EntregaFinal/src/entregafinalsmm/Tool.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package entregafinalsmm;
+
+/**
+ *
+ * @author carlos
+ */
+public enum Tool {
+    TRAZO,
+    LINEA,
+    RECTANGULO,
+    ELIPSE,
+    CURVA
+}
